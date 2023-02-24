0d8330fc3e5d859031a254df2005f53430613a49
Camila <114501675+camilahess@users.noreply.github.com>
2023-02-24 12:22:55 +0100

    Cambio para que sean numeros secuenciales

--- Pruebas/JavaScript/Eventos/ejercicioEventos3.js
+++ Pruebas/JavaScript/Eventos/ejercicioEventos3.js
@@ -24,41 +24,40 @@ window.onload = function() {
     let numFilas = pedirCosas("Introduce el número de filas:");
     let numColumnas = pedirCosas("Introduce el número de columnas:");
 
+  // Crear la tabla
+  let tabla = document.createElement("table");
+  tabla.style.border = "2px solid green";
+  tabla.style.borderCollapse = "collapse";
 
-    // Crear la tabla
-    let tabla = document.createElement("table");
-    tabla.style.border = "2px solid green";
-    tabla.style.borderCollapse = "collapse";
-  
-    // Crear las filas y columnas de la tabla
-    for (let i = 1; i <= numFilas; i++) {
-      let fila = document.createElement("tr");
-  
-      for (let j = 1; j <= numColumnas; j++) {
-        let celda = document.createElement("td");
+  // Crear las filas y columnas de la tabla
+  for (let i = 1; i <= numFilas; i++) {
+    let fila = document.createElement("tr");
+
+    for (let j = 1; j <= numColumnas; j++) {
+      let celda = document.createElement("td");
+      celda.style.border = "2px solid green";
+      celda.textContent = (i - 1) * numColumnas + j;
+
+      // Multiplicar por 2 el valor de la celda al hacer clic
+      celda.addEventListener("click", function() {
+        celda.textContent *= 2;
+      });
+
+      // Cambiar el borde al pasar el ratón
+      celda.addEventListener("mouseover", function() {
+        celda.style.border = "10px solid red";
+      });
+
+      // Restaurar el borde al sacar el ratón
+      celda.addEventListener("mouseout", function() {
         celda.style.border = "2px solid green";
-        celda.textContent = i * j;
-  
-        // Multiplicar por 2 el valor de la celda al hacer clic
-        celda.addEventListener("click", function() {
-          celda.textContent *= 2;
-        });
-  
-        // Cambiar el borde al pasar el ratón
-        celda.addEventListener("mouseover", function() {
-          celda.style.border = "10px solid red";
-        });
-  
-        // Restaurar el borde al sacar el ratón
-        celda.addEventListener("mouseout", function() {
-          celda.style.border = "2px solid green";
-        });
-  
-        fila.appendChild(celda);
-      }
-  
-      tabla.appendChild(fila);
+      });
+
+      fila.appendChild(celda);
     }
-  
-    document.body.appendChild(tabla);
-  };
+
+    tabla.appendChild(fila);
+  }
+
+  document.body.appendChild(tabla);
+};
